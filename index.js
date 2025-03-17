3e6899a734bda2a807826e5c7206eb0c9e66ba53
Glitch (fern-supreme-domain) <none>
2025-03-17 02:36:45 +0000

    🍔🔦 Checkpoint ./index.js:63745502/775

--- index.js
+++ index.js
@@ -47,7 +47,10 @@ app.get(['/w/:id', '/videores/:id'], async (req, res) => {
         '0': [
             'https://siawaseok-wakame-server2.glitch.me',
         ],
-        '1': 'https://pointy-outgoing-basket.glitch.me',
+      '1': 'https://siawaseok-wakame-server1.glitch.me',
+        ’1’： ’https：//panoramic―power―repair.glitch.me’,
+        ’2’： ’https://distinct-coherent-utahraptor.glitch.me’,
+      
     };
 
     if (!/^[a-zA-Z0-9_-]{11}$/.test(videoId)) {
